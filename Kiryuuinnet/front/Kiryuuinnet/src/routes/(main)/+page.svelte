48f1ba6dd3945a1619a1696d9bca6a57b32b2a1b
KiryuuinNFB <doraemonstichisnoon@gmail.com>
2025-06-02 23:35:56 +0700

    added about me page

--- Kiryuuinnet/front/Kiryuuinnet/src/routes/(main)/+page.svelte
+++ Kiryuuinnet/front/Kiryuuinnet/src/routes/(main)/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	let sitetitle = "Kafkarchive";
+	let sitetitle = "Kiryuuinnet";
 	import Heading from "../head.svelte"
 </script>
 
@@ -7,7 +7,7 @@
 
 <body>
     <div class="content">
-        <h1 class="topic">Kiryuuin.net</h1>
-        <p class="walloftext">ดดดด</p>
+        <h1 class="topic">site title</h1>
+        <p class="walloftext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis expedita, unde necessitatibus fuga aperiam dolores temporibus voluptatibus voluptatem ducimus dignissimos! Voluptatibus deleniti minus laboriosam ipsam reiciendis et, fugiat, sequi repellendus ullam voluptatum iure fuga corporis beatae atque natus blanditiis velit, ut nisi saepe eius eligendi veritatis? Animi quia ab tenetur ipsa necessitatibus suscipit? Ut animi rerum vero, necessitatibus nisi ullam.</p>
     </div>
 </body>
---
+++ Kiryuuinnet/front/Kiryuuinnet/src/routes/(main)/empty/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	let sitetitle = "Empty | Kiryuuinnet";
+	import Heading from "../../head.svelte"
+</script>
+
+<Heading bind:SiteHead={sitetitle}/>
+
+<body>
+	<div class="content">
+		<h1 class="topic">Empty page</h1>
+        <p class="walloftext">This is a template page lmao how did you find this</p>
+
+	</div>
+</body>
