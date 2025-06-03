d971ce4ef4d9dc2de9bbd01e523632d97ca2b80d
KiryuuinNFB <doraemonstichisnoon@gmail.com>
2025-06-04 02:33:54 +0700

    wrote about me

--- Kiryuuinnet/front/Kiryuuinnet/src/routes/(main)/about/+page.svelte
+++ Kiryuuinnet/front/Kiryuuinnet/src/routes/(main)/about/+page.svelte
@@ -8,11 +8,33 @@
 <body>
 	<div class="content">
 		<h1 class="topic">About</h1>
-		<h2 class="subtopic">About me</h2>
+		<h2 class="subtopic">Overview</h2>
+		<div class="imagecontainer">
+				<img src="https://media.discordapp.net/attachments/645528014691631105/1379522998624850011/image.png?ex=68408c69&is=683f3ae9&hm=04f32d4edf4ec17fb5d91c9cb2265ff4ac7178f4d03baba3645c3567169de0a0&=&format=webp&quality=lossless" alt="Kiryuuin /NFB Production" class="image">
+			</div>
 		<div class="walloftext">
 			<p>
 				Hi I'm Kiryuuin. You might know me from the remixes I posted on my <a href="https://www.youtube.com/@KiryuuinNFB" class="walloftextlink" target="_blank">Youtube channel</a>.
-				I am interested in music production, programming and 3D modelling.
+				I have been making music since grade 6 and I think I'm somewhat good at it. I usually do morlum, eurobeat, J-Pop and other genres I like listening to. You can commission me at the <a href="/commission" class="walloftextlink">commission page</a>.
+				I'm also interested in programming, especially web development. although I'm not good at it, I am always looking for improvement. I also made <a href="https://github.com/KiryuuinNFB/Kafkarchive" class="walloftextlink" target="_blank">Kafkarchive</a>.
+				A rough character build calculator and database for Honkai: Star Rail. 
+			</p>
+			<p>
+				Before this section ends I wanna tell you (the reader) that.
+				Some stuff I've written in this website might seems nonsense or random unnecessary stuff.
+				That's because even though I'm an introverted person, I do have a lot I wanna talk about and my website seems like a perfect place to do so.
+			</p>
+		</div>
+		<h2 class="subtopic">About me personally</h2>
+		<div class="imagecontainer">
+			<img src="https://cdn.discordapp.com/attachments/645528014691631105/1379535374422118511/20250301_013929.jpg?ex=684097ef&is=683f466f&hm=b6a2bc9732dbda5458c260e17a9b788545d4a2a5b2d91e2bfc03bfb6a79b4c18&" alt="my legs and my pc" class="image" height="500">
+		</div>
+		<div class="walloftext">
+			<p>
+				I'm currently studying in grade 12 and is looking to be a computer science major (Yes I'm a femboy but not because I like computers).
+				I'm not really of a social person and I'm not really of an assertive person either. I mostly spend my time at home either playing video games or doing my hobbies that I stated eariler.
+				I do really like cosplaying, Although I don't have much money for me to spend on it.
+				That's all I want you to know about me.
 			</p>
 		</div>
 	</div>
